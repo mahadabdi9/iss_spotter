6d8fd06399e5dfd22eaa9bca27ce72f85e82e6e8
mahadabdi9 <mahadabdi9@gmail.com>
2023-03-06 10:16:17 -0500

    iss spotter + with promise

---
+++ index.js
@@ -0,0 +1,35 @@
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
+
+const nextISSTimesForMyLocation = function (callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    fetchCoordsByIP(ip, (error, coords) => {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+
+      fetchISSFlyOverTimes(coords, (error, passes) => {
+        if (error) {
+          callback(error, null);
+          return;
+        }
+
+        callback(null, passes);
+      });
+    });
+  });
+};
+
+nextISSTimesForMyLocation((error, passes) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log("It worked! Returned flyover times:", passes);
+});
